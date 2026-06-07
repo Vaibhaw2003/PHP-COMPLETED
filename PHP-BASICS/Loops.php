11dc27637362e08fb42c072a5e7d34706240d756
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-06-07 13:14:10 +0530

    Foreach Loop Example

--- PHP-BASICS/Loops.php
+++ PHP-BASICS/Loops.php
@@ -1,5 +1,27 @@
-<?php
+<!-- <?php
+$name = "John";
+$age = 30;
 for ($i = 0; $i < 10; $i++) {
     echo "The number is: $i<br>";
+    echo "Name: $name, Age: $age<br>";
+
 }
-?>
+?> -->
+
+
+// Foreach Loop Example -------
+
+<!-- <?php
+$fruits = array("Apple", "Banana", "Cherry");
+foreach ($fruits as $fruit) {
+    echo "I like $fruit<br>";
+}
+?> -->
+
+<!-- While Loop Example -------
+ <?php 
+ $i = 0;
+ while($i<5){
+    echo "The number is: $i<br>";
+    $i++;
+ }
