b637972bcf12533c95d51eb5ca1ef66e72d8badc
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-06-09 23:24:59 +0530

    Arithmatic Operators

---
+++ PHP-BASICS/Arithmatics.php
@@ -0,0 +1,15 @@
+<?php
+// Arithmatic Operators
+$a = 34;
+$b = 56;
+// Addition, Subtraction, Multiplication, Division, Modulus
+$output = $a - $b;
+echo $output;
+echo "<br>";
+$output = $a + $b;
+echo $output;
+echo "<br>";
+$output = $a * $b;
+echo $output;
+echo "<br>";
+?>
