ed6eb0ab874dcb18cb552852bf5e474c19721435
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-06-14 02:24:45 +0530

    Array in php

--- PHP-BASICS/Function.php
+++ PHP-BASICS/Function.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
 function add($x, $y){
     $a = $x;
@@ -8,7 +8,29 @@ function add($x, $y){
 }
 
 
+function diff($a, $b){
+    $di = $a -$b;
+    echo $di;
+}
+
+
+// 1. simple function. 
+// 2. augument.
+// 3. return types.
+
 add(12,33);
+diff(23,44);
 
-?>
+?> -->
 
+<!-- <?php
+function adds($s, $v){
+    $output = $s +$v;
+    return $output;
+}
+
+$result =adds(23,45);
+
+echo $result;
+
+?> -->
